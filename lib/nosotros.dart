ea2b5f95e1a9ca7af097a121d7ea7718cf69893a
Gerson <108413232+adonay2003@users.noreply.github.com>
2023-10-02 22:55:06 -0500

    archivo nosotros.dart actualizado

--- lib/nosotros.dart
+++ lib/nosotros.dart
@@ -5,23 +5,27 @@ class NosotrosScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Acerca de Nosotros'),
+        title: Text('Acerca de Nosotros'), // Título de la página
       ),
       body: Container(
-        decoration: BoxDecoration(
-
-        ),
+        decoration: BoxDecoration(), // Decoración del contenedor (puede dejarse en blanco)
         child: ListView(
           children: [
             _buildEquipoItem(
-              'Gerson Adonay Melara Campos',
-              'gerson.melara22@itca.edu.sv',
-              'images/gerson.jpeg',
+              context,
+              'Gerson Adonay Melara Campos', // Nombre
+              'gerson.melara22@itca.edu.sv', // Correo
+              'images/gerson.jpeg', // Ruta de la imagen
+              'Apellido: Melara Campos', // Detalle 1
+              'Edad: 19 años', // Detalle 2
             ),
             _buildEquipoItem(
-              'Emilio Alexander Rodriguez Rivera',
-              'emilio.rodriguez22@itca.edu.sv',
-              'images/emilio.jpeg',
+              context,
+              'Emilio Alexander Rodriguez Rivera', // Nombre
+              'emilio.rodriguez22@itca.edu.sv', // Correo
+              'images/emilio.jpeg', // Ruta de la imagen
+              'Apellido: Rodriguez Rivera', // Detalle 1
+              'Edad: 23 años', // Detalle 2
             ),
           ],
         ),
@@ -29,14 +33,85 @@ class NosotrosScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildEquipoItem(String nombre, String correo, String imagePath) {
-    return ListTile(
-      leading: CircleAvatar(
-        radius: 30,
-        backgroundImage: AssetImage(imagePath),
+  Widget _buildEquipoItem(
+      BuildContext context,
+      String nombre,
+      String correo,
+      String imagePath,
+      String detalles1,
+      String detalles2,
+      ) {
+    return GestureDetector(
+      onTap: () {
+        _showEquipoDetails(context, nombre, correo, imagePath, detalles1, detalles2);
+      },
+      child: Container(
+        margin: EdgeInsets.all(16.0), // Margen alrededor del contenedor
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage(imagePath), // Imagen circular
+            ),
+            SizedBox(height: 10), // Espacio vertical
+            Text(
+              nombre,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold), // Nombre en negrita
+            ),
+            Text(correo), // Correo
+          ],
+        ),
       ),
-      title: Text(nombre),
-      subtitle: Text(correo),
+    );
+  }
+
+  void _showEquipoDetails(
+      BuildContext context,
+      String nombre,
+      String correo,
+      String imagePath,
+      String detalles1,
+      String detalles2,
+      ) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Detalles del Equipo'), // Título del cuadro de diálogo
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage(imagePath), // Imagen circular
+              ),
+              SizedBox(height: 10), // Espacio vertical
+              Text(
+                nombre,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold), // Nombre en negrita
+              ),
+              Text(correo), // Correo
+              SizedBox(height: 10), // Espacio vertical
+              Text(
+                detalles1,
+                style: TextStyle(fontWeight: FontWeight.bold), // Detalle 1 en negrita
+              ),
+              Text(
+                detalles2,
+                style: TextStyle(fontWeight: FontWeight.bold), // Detalle 2 en negrita
+              ),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Botón para cerrar el cuadro de diálogo
+              },
+              child: Text('Cerrar'), // Texto del botón
+            ),
+          ],
+        );
+      },
     );
   }
 }
